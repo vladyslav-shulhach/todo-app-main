fa0afa35308354704c90baf45155233d65c37fbb
Vladyslav <vladyslavshulgach@gmail.com>
2025-07-30 12:14:14 +0300

    Set light theme as default and adjust color styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./styles/main.scss";
 import App from "./App.tsx";
 
+document.documentElement.setAttribute("data-theme", "light");
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
